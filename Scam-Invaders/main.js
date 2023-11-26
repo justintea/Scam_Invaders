d5cbcc73608f2a30303dcce4f73200e4cfdbe3de
justintea <justin.tehhy@gmail.com>
2023-11-27 03:59:26 +0800

    20231127 basic UI

--- Scam-Invaders/main.js
+++ Scam-Invaders/main.js
@@ -1,13 +1,30 @@
  /*----- constants -----*/
-
+ const diffType = ['Easy','Medium','Hard'];
+ const difficulty = diffType[1];
 
  /*----- state variables -----*/
-
+let playerscore = 5 ; 
 
  /*----- cached elements  -----*/
 
 
  /*----- event listeners -----*/
+const diffButton = () => 
+{ document.querySelector('#medButton').addEventListener('click', handleMode('med'))      };
 
+
+
+
+
+ /*----- render functions -----*/
+const render = () => {};
 
  /*----- functions -----*/
+
+const main = () => {
+diffButton();
+render();
+ };
+
+ //* main operating
+main();
